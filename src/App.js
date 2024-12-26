12912460885513119568b18e79f7a0a9a1f51e7b
MahaGit1233 <maharush5409@gmail.com>
2024-12-26 15:15:41 +0530

    loader commit

--- src/App.js
+++ src/App.js
@@ -4,25 +4,34 @@ import MoviesList from './Components/MoviesList';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
+
     const response = await fetch('https://swapi.py4e.com/api/films/')
-    const data = await response.json();
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transformedMovies);
+      const data = await response.json();
+      setTimeout(() => {
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    },2000);
   }
 
   return (
     <React.Fragment>
       <section className='button'>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section className='load'>
+        {isLoading && <div>Loading...</div>}
       </section>
       <section className='movies'>
         <MoviesList movies={movies} />
